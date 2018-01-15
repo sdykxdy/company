0d9d4261205bc92fbf4abbfc41d1c345b063d149
xudeyin <xudeyin@315pr.com>
2018-01-15 11:31:16 +0800

    commint

---
+++ src/main/java/com/toysuperman/toysupermanproject/ToysupermanProjectApplication.java
@@ -0,0 +1,12 @@
+package com.toysuperman.toysupermanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysupermanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysupermanProjectApplication.class, args);
+	}
+}
