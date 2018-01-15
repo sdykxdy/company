808f3ae8f7c2514689eec1a73fb419c9bdd3a650
xudeyin <xudeyin@315pr.com>
2018-01-15 11:30:47 +0800

    commint

---
+++ src/test/java/com/toysuperman/toysupermanproject/ToysupermanProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toysuperman.toysupermanproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToysupermanProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
