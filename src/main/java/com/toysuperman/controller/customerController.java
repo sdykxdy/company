74d5ace9a731e05c08872b1e5f8f905594b56503
xudeyin <xudeyin@315pr.com>
2018-01-15 11:31:33 +0800

    commint

---
+++ src/main/java/com/toysuperman/controller/customerController.java
@@ -0,0 +1,13 @@
+package com.toysuperman.controller;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: xudeyin
+ * \* Date: 2018/1/15
+ * \* Time: 11:29
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description: xudeyin
+ * \
+ */
+public class customerController {
+}
